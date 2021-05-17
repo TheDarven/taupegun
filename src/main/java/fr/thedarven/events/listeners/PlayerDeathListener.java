4601df6f09f73e6a07843ccbc2325d9348173cb2
TheDarven <lucas.minimoys@hotmail.fr>
2021-05-17 19:59:36 +0200

    Refactored the code

--- src/main/java/fr/thedarven/events/listeners/PlayerDeathListener.java
+++ src/main/java/fr/thedarven/events/listeners/PlayerDeathListener.java
@@ -66,39 +66,39 @@ public class PlayerDeathListener implements Listener {
 			Bukkit.broadcastMessage(deathAllMessage);
 		}
 
+		Player deadPlayer = pl.getPlayer();
 		if (EnumGameState.isCurrentState(EnumGameState.GAME)){
 			pl.setAlive(false);
 			
 			/* ON S'OCCUPE DU JOUEUR */
 			TeamCustom team = pl.getTeam();
 			this.main.getPlayerManager().sendPlaySound(Sound.WITHER_SPAWN);
-			
+
 			if (this.main.getGameManager().molesEnabled() && pl.isOnline()){
 				Map<String, String> params = new HashMap<>();
 				params.put("playerName", pl.getName());
 				String headName = "§6" + TextInterpreter.textInterpretation(LanguageBuilder.getContent("ITEM", "head", true), params);
 
-				if (Objects.nonNull(pl.getPlayer())) {
+				if (Objects.nonNull(deadPlayer)) {
 					if (this.main.getScenariosManager().goldenHead.getValue() > 0) {
-						pl.getPlayer().getWorld().dropItem(pl.getPlayer().getLocation(), this.main.getPlayerManager().getHeadOfPlayer(pl.getName(), headName));
+						deadPlayer.getWorld().dropItem(deadPlayer.getLocation(), this.main.getPlayerManager().getHeadOfPlayer(pl.getName(), headName));
 					}
 
 					int nbGoldenApple = this.main.getScenariosManager().deathGoldenApple.getIntValue();
 					if (nbGoldenApple > 0) {
-						pl.getPlayer().getWorld().dropItem(pl.getPlayer().getLocation(), new ItemStack(Material.GOLDEN_APPLE, nbGoldenApple));
+						deadPlayer.getWorld().dropItem(deadPlayer.getLocation(), new ItemStack(Material.GOLDEN_APPLE, nbGoldenApple));
 					}
 				}
 			}
-			
-			Player p = pl.getPlayer();
-			if (Objects.nonNull(p)){
-				p.getPlayer().setGameMode(GameMode.SPECTATOR);
+
+			if (Objects.nonNull(deadPlayer)){
+				deadPlayer.setGameMode(GameMode.SPECTATOR);
 				World world = this.main.getWorldManager().getWorld();
 				if (Objects.nonNull(world)) {
-					p.getPlayer().teleport(new Location(world,0,200,0));
+					deadPlayer.teleport(new Location(world,0,200,0));
 				}
-				p.getPlayer().sendMessage("§c" + LanguageBuilder.getContent("EVENT_DEATH", "deathMumble", true));
-				p.getPlayer().sendMessage("§c" + LanguageBuilder.getContent("EVENT_DEATH", "deathInfo", true));
+				deadPlayer.sendMessage("§c" + LanguageBuilder.getContent("EVENT_DEATH", "deathMumble", true));
+				deadPlayer.sendMessage("§c" + LanguageBuilder.getContent("EVENT_DEATH", "deathInfo", true));
 			}
 			
 			this.main.getDatabaseManager().updateMoleDeath(pl.getUuid().toString(), 1);
